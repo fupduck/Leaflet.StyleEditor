9d5bb95946812874f669110144699f36705f41d8
Fup Duck <fupduck@sacknagel.com>
2018-05-16 23:45:34 +0200

    config git

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 grunt
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git remote add origin https://${GITHUB_TOKEN}@github.com/fupduck/Leaflet.StyleEditor.git > /dev/null 2>&1
+
 git add -u
 git commit -m "minify css & js"
-git push
+
+git push --quiet --set-upstream origin master
